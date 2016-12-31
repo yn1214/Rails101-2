6556087a24dc2b1e2c2b67a015248eeac04d1c11
yn1214 <q11a22z33w44@lingjiayangdeMacBook-Pro.local>
2016-12-31 22:23:41 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,14 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
+
 
   def create
     @group = Group.new(group_params)
